6ae5812e45b645a10efa5da8e960267861c48a85
Brandon Miller <millbj92@gmail.com>
2021-09-28 13:10:35 -0400

    app structure

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
+//Application configurations
+type App struct {
+}
+
+//Sets up the application.
+func (app *App) Run() error {
+	fmt.Println("Setting up Applicaiton")
+
+	return nil
+}
+
 func main() {
-	
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up Rest API")
+		fmt.Println(err)
+	}
 }
